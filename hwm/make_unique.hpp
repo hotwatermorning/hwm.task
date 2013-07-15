c20b4e79c98941b82ca89740d7506df74d4da50f
hotwatermorning <hotwatermorning@gmail.com>
2013-07-16 01:06:57 +0900

    hide helper into detail.

--- hwm/make_unique.hpp
+++ hwm/make_unique.hpp
@@ -16,23 +16,27 @@
 
 namespace hwm {
 
-template <class T, class... Args>
-std::unique_ptr<T> make_unique_helper(std::false_type, Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
+namespace detail { namespace ns_make_unique {
 
-template <class T, class... Args>
-std::unique_ptr<T> make_unique_helper(std::true_type, Args&&... args) {
-     static_assert(std::extent<T>::value == 0,
-         "make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+    template <class T, class... Args>
+    std::unique_ptr<T> make_unique_helper(std::false_type, Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
 
-     typedef typename std::remove_extent<T>::type U;
-     return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
-}
+    template <class T, class... Args>
+    std::unique_ptr<T> make_unique_helper(std::true_type, Args&&... args) {
+         static_assert(std::extent<T>::value == 0,
+             "make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+
+         typedef typename std::remove_extent<T>::type U;
+         return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
+    }
+
+}}  //namespace detail::ns_make_unique
 
 template <class T, class... Args>
 std::unique_ptr<T> make_unique(Args&&... args) {
-     return make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
+     return detail::ns_make_unique::make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
 }
 
 }   //namespace hwm
