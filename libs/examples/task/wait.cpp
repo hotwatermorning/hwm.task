44e05e664dfb3129366f3d5c749f5381e4d097b5
hotwatermorning <hotwatermorning@gmail.com>
2015-03-04 14:54:28 +0900

    add output messages

--- libs/examples/task/wait.cpp
+++ libs/examples/task/wait.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <hwm/task/task_queue.hpp>
+#include "../utils/stream_mutex.hpp"
 
 //! wait()メンバ関数でタスクが全て終了するのを待つサンプル
 
@@ -13,13 +14,15 @@ int main()
     hwm::task_queue tq(3);
 
     for(int i = 0; i < 3; ++i) {
-        tq.enqueue([]{
+        tq.enqueue([i]{
+            hwm::mcout << ">>> Execute task[" << i << "]" << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(1));
+            hwm::mcout << "<<< Execute task[" << i << "]" << std::endl;
         });
     }
 
-    std::cout << ">>> waiting for 3 tasks to be finished" << std::endl;
+    hwm::mcout << ">>> waiting for 3 tasks to be finished" << std::endl;
     tq.wait();
-    std::cout << "<<< finished" << std::endl;
+    hwm::mcout << "<<< finished" << std::endl;
 }
 
